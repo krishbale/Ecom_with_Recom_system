37f4127a5c34fc645fb2238d9fc51d28b30c04f9
Balkrishna pokharel <58511308+krishbale@users.noreply.github.com>
2022-12-27 18:56:48 +0545

    nav created

--- aecommercefrontend/src/pages/NavBar.js
+++ aecommercefrontend/src/pages/NavBar.js
@@ -1,9 +1,37 @@
+import { Link } from "@mui/material";
 import * as React from "react";
-
+import '../styles/NavBar.css';
 const NavBar = () => {
   return (
     <>
-<div>NavBar</div>
+  
+      <li className="nav_items active">
+        <Link className="nav_items__item" to="/">Home</Link>
+       
+      </li>
+      <li className="nav_items">
+      <Link className="nav_items__item" to="/about">About</Link>
+
+      </li>
+      <li className="nav_items">
+      <Link className="nav_items__item" to="/contacts">COntact</Link>
+
+      </li>
+  <li className="nav_items">
+  <Link className="nav_items__item" to="/login">Login</Link>
+
+  </li>
+  <li className="nav_items">
+  <Link className="nav_items__item" to="/logout">logout</Link>
+
+  </li>
+  <li className="nav_items">
+  <Link className="nav_items__item" to="/products">Products</Link>
+
+  </li>
+   
+
+
      
     </>
   )
