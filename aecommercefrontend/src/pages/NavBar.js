0df4f65f1e56e8d64f856851e49c56aeead93ddf
Balkrishna pokharel <58511308+krishbale@users.noreply.github.com>
2022-12-27 19:00:54 +0545

    Navbar created

--- aecommercefrontend/src/pages/NavBar.js
+++ aecommercefrontend/src/pages/NavBar.js
@@ -1,32 +1,33 @@
-import { Link } from "@mui/material";
+
 import * as React from "react";
+import { NavLink } from "react-router-dom";
 import '../styles/NavBar.css';
 const NavBar = () => {
   return (
     <>
   
       <li className="nav_items active">
-        <Link className="nav_items__item" to="/">Home</Link>
+        <NavLink className="nav_items__item" to="/">Home</NavLink>
        
       </li>
       <li className="nav_items">
-      <Link className="nav_items__item" to="/about">About</Link>
+      <NavLink className="nav_items__item" to="/about">About</NavLink>
 
       </li>
       <li className="nav_items">
-      <Link className="nav_items__item" to="/contacts">COntact</Link>
+      <NavLink className="nav_items__item" to="/contacts">COntact</NavLink>
 
       </li>
   <li className="nav_items">
-  <Link className="nav_items__item" to="/login">Login</Link>
+  <NavLink className="nav_items__item" to="/login">Login</NavLink>
 
   </li>
   <li className="nav_items">
-  <Link className="nav_items__item" to="/logout">logout</Link>
+  <NavLink className="nav_items__item" to="/logout">logout</NavLink>
 
   </li>
   <li className="nav_items">
-  <Link className="nav_items__item" to="/products">Products</Link>
+  <NavLink className="nav_items__item" to="/products">Products</NavLink>
 
   </li>
    
