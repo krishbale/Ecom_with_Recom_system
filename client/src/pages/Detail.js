7c1703814c750269d5a24e626026329f631be4aa
Balkrishna pokharel <58511308+krishbale@users.noreply.github.com>
2022-12-31 21:15:19 +0545

    singleproductrenderingcompletedandbugfixed

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -6,33 +6,44 @@ import { useParams } from 'react-router-dom'
 
 
 function Detail() {
+  const [loading,isloading] = useState(true);
+  
     const [products ,setProducts]=useState([]);
     useEffect(() => {
      const fetchdata = async () => {
        const { data } = await axios.get('/products');
        setProducts(data.data);
+       isloading(false)
      };
      fetchdata();
    }, []);
    let {id}= useParams()
      let productDetail = products.find(item=> item.id == id )
-     console.log(productDetail)
+     if(loading){
+      return <div className="page_loading">Loading.....</div>;
+     }
+     
     return(
-<h1>{productDetail.title}</h1>
+      <>
+      <h1>{productDetail.title}</h1>
+      <div className='detail'>
+      <h1>{productDetail.id}</h1>
+      <div className="image">
+        <img src={productDetail.image}/> 
+      </div>
+      <div className="description">
+        <h1>{productDetail.title}</h1>
+        <p>{productDetail.description}</p>
+        <h3>{productDetail.price}</h3>
+      </div>
+      </div>
+
+      </>
+
+
     )
  
 }
 
 export default Detail
 
-//  <div className='detail'>
-//   <h1>{productDetail.id}</h1>
-// <div className="image">
-//   <img src={productDetail.image}/> 
-// </div>
-// <div className="description">
-//   <h1>{productDetail.title}</h1>
-//   <p>{productDetail.description}</p>
-//   <h3>{productDetail.price}</h3>
-// </div>
-// </div>
