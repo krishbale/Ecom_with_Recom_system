10536570f967d202b0719bc4985769ae310ffe7b
balkrishna pokharel <sushil.pokharel.7528@gmail.com>
2023-01-10 16:16:20 +0545

    updatedproduct

---
+++ server/controller/products.js
@@ -0,0 +1,13 @@
+const data = require("../ProductItem/product.json")
+
+
+const products = async (req,res,next)=>{
+   
+
+    res.status(200).json({
+        success:true,
+        data,
+        
+    })
+}
+ module.exports = products
