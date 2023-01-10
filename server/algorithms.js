90ff33ab5ebd38d484dbab6fe12b99893f1b328a
Bishnu Upadhyay <bishnuupadhyay012@gmail.com>
2023-01-10 18:02:23 +0545

    removing Stop words

--- server/algorithms.js
+++ server/algorithms.js
@@ -54,9 +54,14 @@ function tfidf(docs) {
     // Loop through each document
     for (let i = 0; i < docs.length; i++) {
       // Split the document into an array of terms
-      let terms = docs[i].split(' ');
-  
+      let split = docs[i].split(' ');
+    
+      let stopwords = ["i", "me", "my","With", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+
+// Filtering out the stopwords
+let terms = split.filter(word => !stopwords.includes(word));  
       // Calculate the term frequency for each term in the document
+      console.log(terms)
       for (let j = 0; j < terms.length; j++) {
         let term = terms[j];
         if (termFreq[term]) {
