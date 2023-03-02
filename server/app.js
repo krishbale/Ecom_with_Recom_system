31b36a9f5f7ae25d35fc8dc0db1b7873511af778
balkrishna pokharel <sushil.pokharel.7528@gmail.com>
2023-03-02 13:44:01 +0545

    mongodbsession

--- server/app.js
+++ server/app.js
@@ -10,17 +10,17 @@ const connectDB = require('./db/connect');
 app.use(express.json())
 const User = require('./models/userSchema');
 
-const MongoDBStore = require('connect-mongodb-session')
-(session)
+const MongoDBStore = require('connect-mongo')
+
 
 const PORT = process.env.PORT || 5000;
 const DB = process.env.MONGODB_URL;
 
 const MAX_AGE = 1000 * 60 * 60 * 24 // 3hrs
 //connecting to the settin up connect -mongodb-session store
-const MongoDBstore = new MongoDBStore({
-    uri:DB,
-    collection:'mySessions',
+const MongoDBstore = MongoDBStore.create({
+    mongoUrl:DB,
+    collectionName:'mySessions',
 })
 app.use(session({
     secret:'a1s2d3f4g5h6',
